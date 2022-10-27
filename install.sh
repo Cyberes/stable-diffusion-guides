08a43a78f6d1807d22347b576915168ef240f2c7
adpkadspokasdk <64224601+Cyberes@users.noreply.github.com>
2022-10-27 11:15:31 -0600

    remove

--- install.sh
+++ install.sh
@@ -5,4 +5,3 @@ TMP=$(mktemp)
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb -O "$TMP"
 sudo apt install -y xfonts-base xfonts-75dpi fonts-baekmuk fonts-dejima-mincho fonts-nanum-coding fonts-takao fonts-takao-pgothic fonts-takao-gothic fonts-takao-mincho
 sudo dpkg -i "$TMP"
-
