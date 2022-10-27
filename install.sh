e7a5386b3806bcd751d1fa2aa8ea3465943cbd69
adpkadspokasdk <64224601+Cyberes@users.noreply.github.com>
2022-10-27 11:12:07 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 # Don't install with apt. It segfaults
 TMP=$(mktemp)
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb -O "$TMP"
-sudo apt install -y xfonts-base xfonts-75dpi ttf-wqy-microhei ttf-wqy-zenhei ttf-dejima-mincho ttf-umefon ttf-takao ttf-takao-gothic ttf-takao-mincho ttf-takao-pgothic ttf-kochi-gothic ttf-kochi-mincho ttf-sazanami-gothic ttf-sazanami-mincho ttf-nanum-coding fonts-nanum fonts-nanum-extra ttf-baekmuk 
+sudo apt install -y xfonts-base xfonts-75dpi fonts-baekmuk fonts-dejima-mincho fonts-nanum-coding fonts-takao fonts-takao-pgothic fonts-takao-gothic fonts-takao-mincho
 sudo dpkg -i "$TMP"
+
