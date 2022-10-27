10c2ff6d508bd4e629c93c0bc7cd89deb6ec3621
adpkadspokasdk <64224601+Cyberes@users.noreply.github.com>
2022-10-27 10:22:19 -0600

    work

--- update-guides.sh
+++ update-guides.sh
@@ -5,22 +5,27 @@
 # sudo apt install xfonts-75dpi
 # sudo dpkg -i wkhtmltox_0.12.6-1.focal_amd64.deb
 
-wget "https://rentry.org/voldy/pdf" -O "archives/VOLDY RETARD GUIDE.pdf"
-wget "https://rentry.org/drfar/pdf" -O "archives/Inpainging and Outpainting.pdf"
-wget "https://rentry.org/textard/pdf" -O "archives/RETARD'S GUIDE TO TEXTUAL INVERSION.pdf"
-wget "https://rentry.org/anime_and_titties/pdf" -O "archives/big titty anon's list of artists.pdf"
-wget "https://rentry.org/informal-training-guide/pdf" -O "archives/Informal Training Guide.pdf"
-wget "https://rentry.org/865dy/pdf" -O "archives/Getting Started on Paperspace.pdf"
-wget "https://rentry.org/cputard/pdf" -O "archives/CPU RETARD GUIDE.pdf"
-wget "https://rentry.org/sd-nativeisekai/pdf" -O "archives/Stable Diffusion Native Isekai.pdf"
-wget "https://rentry.org/ayymd-stable-diffustion-v1_4-guide/pdf" -O "archives/AyyMD Stable Diffuse v1.4 for Wangblows 10.pdf"
-wget "https://rentry.org/sdamd/pdf" -O "archives/Stable Diffusion AMD.pdf"
-wget "https://rentry.org/yrpvv/pdf" -O "archives/Stable Diffusion Models.pdf"
-wget "https://rentry.org/sdupscale/pdf" -O "archives/Larger resolutions with Stable Diffusion.pdf"
-wget "https://rentry.org/male-to-anime-girl/pdf" -O "archives/How to Turn Yourself Into a Cute Anime Girl.pdf"
-wget "https://rentry.org/sdmodels/pdf" -O "archives/Stable Diffusion Models.pdf"
-wget "https://rentry.org/sdg_FAQ" -O "archives/sdg FAQ.pdf"
+download() {
+	for i in {1...10}; do
+		wget -T 15 -c "$1" "$2" && break
+	done
+}
+
+download "https://rentry.org/voldy/pdf" "archives/VOLDY RETARD GUIDE.pdf"
+download "https://rentry.org/drfar/pdf" "archives/Inpainging and Outpainting.pdf"
+download "https://rentry.org/textard/pdf" "archives/RETARD'S GUIDE TO TEXTUAL INVERSION.pdf"
+download "https://rentry.org/anime_and_titties/pdf" "archives/big titty anon's list of artists.pdf"
+download "https://rentry.org/informal-training-guide/pdf" "archives/Informal Training Guide.pdf"
+download "https://rentry.org/865dy/pdf" "archives/Getting Started on Paperspace.pdf"
+download "https://rentry.org/cputard/pdf" "archives/CPU RETARD GUIDE.pdf"
+download "https://rentry.org/sd-nativeisekai/pdf" "archives/Stable Diffusion Native Isekai.pdf"
+download "https://rentry.org/ayymd-stable-diffustion-v1_4-guide/pdf" "archives/AyyMD Stable Diffuse v1.4 for Wangblows 10.pdf"
+download "https://rentry.org/sdamd/pdf" "archives/Stable Diffusion AMD.pdf"
+download "https://rentry.org/yrpvv/pdf" "archives/Stable Diffusion Models.pdf"
+download "https://rentry.org/sdupscale/pdf" "archives/Larger resolutions with Stable Diffusion.pdf"
+download "https://rentry.org/male-to-anime-girl/pdf" "archives/How to Turn Yourself Into a Cute Anime Girl.pdf"
+download "https://rentry.org/sdmodels/pdf" "archives/Stable Diffusion Models.pdf"
+download "https://rentry.org/sdg_FAQ" "archives/sdg FAQ.pdf"
 
 wkhtmltopdf "https://wiki.installgentoo.com/wiki/Stable_Diffusion" "archives/wiki.installgentoo.com Stable Diffusion Guide.pdf"
-wkhtmltopdf "https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Run-with-Custom-Parameters" "archives/Run with Custom Parameters - stable-diffusion-webui wiki.pdf"
 # wkhtmltopdf "https://proximacentaurib.notion.site/A-guide-to-getting-started-with-the-paperspace-port-of-AUTOMATIC1111-s-web-UI-for-ppl-who-get-nervou-b83c2213f17e452e8b0e37ba64fe9758" "Getting started with the paperspace port of AUTOMATIC1111s web UI for ppl who get nervous.pdf"
