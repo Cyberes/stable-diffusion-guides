5974a74c2373299c01f1dfcd8c6747040b90e606
adpkadspokasdk <64224601+Cyberes@users.noreply.github.com>
2022-10-27 10:13:26 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 # Don't install with apt. It segfaults
 TMP=$(mktemp)
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb -O "$TMP"
-sudo apt install xfonts-75dpi
+sudo apt install xfonts-base xfonts-75dpi
 sudo dpkg -i "$TMP"
