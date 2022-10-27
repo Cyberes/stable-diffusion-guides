ac7cc0a0edd856d23ca1ede53d2f4f5ce916a2bd
adpkadspokasdk <64224601+Cyberes@users.noreply.github.com>
2022-10-27 11:00:13 -0600

    [AUTO] Update guides

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 cd "$GITHUB_WORKSPACE"
 git add --all
-if [ -n `git diff-index --exit-code --ignore-submodules HEAD` ]; then
+if [[ -n $(git diff-index --exit-code --ignore-submodules HEAD) ]]; then
 	git commit -m "[AUTO] Update guides"
 	git push origin main
 fi
